3e24e77323c4301d0217203c2045aa005f37aa55
Ronald Head <ronaldlhead@gmail.com>
2021-10-19 10:52:22 -0600

    Add object variables

--- model-1.js
+++ model-1.js
@@ -13,3 +13,18 @@ var likesCheeseburgers = true;
 var favVideoGames = ["FF7", "Oblivion", "Guardian Heroes", "Spider-Man", "Halo"];
 var favBoardGames = ["Small World", "Ticket to Ride", "Cargo Noir", "Monopoly", "Munchkin"];
 var favAirBnbs = ["Mountain Goat Lodge", "Breckenridge", "Dillon"];
+
+var myMac = {
+  myMac.modelYear = 2020;
+  myMac.RAM = "8GB";
+  myMac.chip = "Apple M1";
+  myMac.OS = "Big Sur";
+}
+
+var favWatch = {
+  favWatch.make = "Timex";
+  favWatch.model = "Waterbury";
+  favWatch.strap = "Leather";
+  favWatch.dial = "Black";
+  favWatch.movement = "Quartz";
+}
