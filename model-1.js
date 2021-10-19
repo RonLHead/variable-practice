79aa1821f4c0e2569f1a66b869117dd95957e22f
Ronald Head <ronaldlhead@gmail.com>
2021-10-19 10:47:28 -0600

    Add array variables

--- model-1.js
+++ model-1.js
@@ -9,3 +9,7 @@ var goalWeight = 185;
 var isHungry = true;
 var likesExercise = false;
 var likesCheeseburgers = true;
+
+var favVideoGames = ["FF7", "Oblivion", "Guardian Heroes", "Spider-Man", "Halo"];
+var favBoardGames = ["Small World", "Ticket to Ride", "Cargo Noir", "Monopoly", "Munchkin"];
+var favAirBnbs = ["Mountain Goat Lodge", "Breckenridge", "Dillon"];
