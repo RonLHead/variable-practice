3961a042997f8557d7c1df7f5c5196aa9c03e044
Ronald Head <ronaldlhead@gmail.com>
2021-10-19 10:40:10 -0600

    Add number variables

--- model-1.js
+++ model-1.js
@@ -1,3 +1,7 @@
 var favBoots = "Red Wing Iron Ranger";
 var favJacket = "Flint and Tinder Waxed Trucker Jacket";
 var favFlannel = "LL Bean Chamois Flannel Shirt";
+
+var favNum = 28;
+var birthYear = 1984;
+var goalWeight = 185;
